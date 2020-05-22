93ce74d087ae393723caa592dde46ce3051edad3
lry <lry@example.com>
2020-05-22 11:45:54 +0800

    尝试pycharm直接提交

--- bwOnline/settings.py
+++ bwOnline/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'course',
+    'organization',
+    'operation',
 ]
 
 MIDDLEWARE = [
